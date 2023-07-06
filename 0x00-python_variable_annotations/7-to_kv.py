4691e1edf511f984a31a2c6a9c81b2d9c37a3059
joyogbu <obyjoy@yahoo.com>
2023-07-06 09:31:12 -0700

    task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -2,9 +2,9 @@
 '''take a string and integer/float and return a tuple'''
 
 
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''defining the function'''
     return (k, v*v)
