b45b94934f803482614803106b8884a289a35b97
joyogbu <obyjoy@yahoo.com>
2023-07-06 05:38:34 -0700

    task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''mixed list annotations'''
+
+
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+        total = 0.0
+        for num in mxd_lst:
+            total += num
+        return (total)
