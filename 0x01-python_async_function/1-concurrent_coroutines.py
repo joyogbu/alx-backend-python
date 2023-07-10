bf336009684573e3e045d5ca26ef020b7e5e78e4
joyogbu <obyjoy@yahoo.com>
2023-07-10 04:36:47 -0700

    task 0

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,12 +4,13 @@
 
 import random
 import asyncio
+from typing import List
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     '''defining the function'''
     while True:
         for i in range(n):
