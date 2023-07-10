df71dc3adbd5dcd762cc869e7fd5b4fb187ff42c
joyogbu <obyjoy@yahoo.com>
2023-07-10 05:38:42 -0700

    task 0

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -19,7 +19,6 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
             group.append(asyncio.create_task(wait_random(max_delay)))
             # li = await asyncio.gather(wait_random(max_delay))
             li = await asyncio.gather(*group)
-        if li == []:
-            return (None)
-        # await asyncio.sleep(max_delay)
+        if li == None:
+            return []
         return (li)
