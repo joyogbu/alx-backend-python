e14087efc538c6c8c0133a1fe53c6fe818af2aed
joyogbu <obyjoy@yahoo.com>
2023-07-10 12:40:48 -0700

    task 1 edited

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -12,11 +12,10 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     '''defining the function'''
-    while True:
-        group = []
-        for i in range(n):
-            # await asyncio.sleep(max_delay)
-            group.append(asyncio.create_task(wait_random(max_delay)))
-            # li = await asyncio.gather(wait_random(max_delay))
-            li = await asyncio.gather(*group)
-        return (li)
+    group = []
+    for i in range(n):
+        group.append(asyncio.create_task(wait_random(max_delay)))
+        li = await asyncio.gather(*group)
+        if li == 'None' or li == []:
+            return []
+    return (li)
--- 0x01-python_async_function/1-main.py
+++ 0x01-python_async_function/1-main.py
@@ -9,4 +9,4 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 print(asyncio.run(wait_n(5, 5)))
 print(asyncio.run(wait_n(10, 7)))
 print(asyncio.run(wait_n(10, 0)))
-print(asyncio.run(wait_n(0, 10)))
+#print(asyncio.run(wait_n(0, 10)))
