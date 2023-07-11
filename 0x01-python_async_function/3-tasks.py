67377d4c84e2ce6275de2c95a2799c58fe1f53b0
joyogbu <obyjoy@yahoo.com>
2023-07-10 18:38:06 -0700

    edited task 3

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -9,6 +9,6 @@ from typing import Awaitable, Any, Callable, Coroutine
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Callable[[None], Awaitable[int]]:
+def task_wait_random(max_delay: int) -> asycio.Task:
     '''defining the function'''
     return asyncio.ensure_future(wait_random(max_delay))
