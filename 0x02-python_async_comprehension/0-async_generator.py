b173bd26c77cdc02b2326fc95aaf0494cc7d4612
joyogbu <obyjoy@yahoo.com>
2023-07-11 11:15:23 -0700

    task0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,13 +2,15 @@
 '''co-routine asynchronous generator that takes no argument'''
 
 
-import asyncio, random
+import asyncio
+import random
 from typing import Generator, Iterator
 
 
 async def async_generator() -> Iterator[float]:
+    '''defining the function'''
     i = random.uniform(0, 10)
-    #for i in range(10):
-    #yield i
+    # for i in range(10):
+    # yield i
     await asyncio.sleep(1)
     yield i
