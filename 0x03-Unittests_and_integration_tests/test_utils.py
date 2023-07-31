2263a1f43ba605d1f53e998e83d355562642fef6
joyogbu <obyjoy@yahoo.com>
2023-07-31 10:08:09 +0000

    task 0

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,19 +6,19 @@ import unittest
 from utils import access_nested_map
 
 
-class TestNestedMap(unittest.TestCase):
+class TestAccessNestedMap(unittest.TestCase):
     '''defining a test access_nested_map class'''
-    def test_map1(self):
+    def test_access_nested_map(self):
         '''defining the function test'''
-        nested_map={"a": 1}
+        nested_map = {"a": 1}
         self.assertEqual(access_nested_map(nested_map, ("a")), 1)
 
-    def test_map2(self):
+    def test_access_nested_map(self):
         '''defining the function for test2'''
-        nested_map={"a": {"b": 2}}
-        self.assertEqual(access_nested_map(nested_map, ("a",)), {b: 2})
+        nested_map = {"a": {"b": 2}}
+        self.assertEqual(access_nested_map(nested_map, ("a",)), {'b': 2})
 
-    def test_map3(self):
+    def test_access_nested_map(self):
         '''defining the functiin for test3'''
-        nested_map={"a": {"b": 2}}
+        nested_map = {"a": {"b": 2}}
         self.assertEqual(access_nested_map(nested_map, ("a", "b")), 2)
