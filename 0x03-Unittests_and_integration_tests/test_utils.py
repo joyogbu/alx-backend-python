58a687a1a51a8ff9ff9e4d12596634f271e7bcc0
joyogbu <obyjoy@yahoo.com>
2023-07-31 22:59:15 +0000

    task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,6 +8,7 @@ from unittest import mock
 from unittest.mock import patch, Mock
 from utils import access_nested_map
 from utils import get_json
+from utils import memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -49,5 +50,23 @@ class TestGetJson(unittest.TestCase):
             # mock_get.return_value = mock_response
             mock_response.get.side_effect = test_payload.get
             self.assertEqual(mock_response.get_json(test_url), test_payload)
-        # assert_called_once_with(test_url)
-        # self.assertEqual(test_url, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    '''defining the class for memoixation'''
+    def test_memoize(self):
+        '''defing the test function'''
+        class TestClass:
+            '''defining the class'''
+            def a_method(self):
+                '''defining the function'''
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+            with patch('utils.memoize'):
+                mock_response = Mock()
+                mock_response.a_method.return_value = 42
+                if mock_response.call_count == 2:
+                    self.assertEqual(a_method(), 42)
