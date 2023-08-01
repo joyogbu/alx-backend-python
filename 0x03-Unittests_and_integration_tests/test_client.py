75d1c68b34822bd3e4beb6927389876a3a838d66
joyogbu <obyjoy@yahoo.com>
2023-08-01 04:08:40 +0000

    task 4

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''test module for testing client'''
+
+
+import unittest
+from unittest import mock
+from unittest.mock import patch, Mock
+from utils import access_nested_map, get_json, memoize
+from parameterized import parameterized
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    '''defining the test class for client'''
+    @parameterized.expand([
+        ('google'),
+        ('abc')
+        ])
+    # @mock.patch('utils.get_json')
+    def test_org(self, expected):
+        '''defining the test org function'''
+        with patch('client.GithubOrgClient.org')as p:
+            mock_response = Mock()
+            mock_response.get_json.return_value = expected
+            self.assertEqual(mock_response.org(), expected)
